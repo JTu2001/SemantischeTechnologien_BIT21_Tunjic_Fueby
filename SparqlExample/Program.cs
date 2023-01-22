288c70c054fa3e9eb74d8fef478230e9d61a5df1
michifueby <michi.fueby@gmail.com>
2023-01-22 10:15:45 +0100

    Change query

--- SparqlExample/Program.cs
+++ SparqlExample/Program.cs
@@ -12,46 +12,44 @@
             // Erstellen eines Graphen
             IGraph graph = new Graph();
 
-            // Erstellen des Graphen aus einem String (alternativ kann auch aus einer Datei gelesen werden)
-            string data = "@prefix foaf: <http://xmlns.com/foaf/0.1/> . \n"
-                        + "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n\n"
-                        + "<http://example.org/Michael#me> a foaf:Person .\n"
-                        + "<http://example.org/Michael#me> foaf:name \"Michael\" .\n"
-                        + "<http://example.org/Michael#me> foaf:mbox <mailto:alice@example.org> .\n"
-                        + "<http://example.org/Michael#me> foaf:knows <http://example.org/bob#me> .\n"
-                        + "<http://example.org/Markus#me> foaf:knows <http://example.org/alice#me> .\n"
-                        + "<http://example.org/Markus#me> foaf:name \"Markus\" .\n"
-                        + "<http://example.org/Michael#me> foaf:knows <http://example.org/charlie#me> .\n"
-                        + "<http://example.org/Josip#me> foaf:knows <http://example.org/alice#me> .\n"
-                        + "<http://example.org/Josip#me> foaf:name \"Josip\" .\n"
-                        + "<http://example.org/Michael#me> foaf:knows <http://example.org/snoopy> .\n"
-                        + "<http://example.org/Manuel> foaf:name \"Manuel\"@en .";
-
-            // Parsen des Strings in den Graphen.
-            StringParser.Parse(graph, data);
-
-            // Erstellen des SPARQL Ausdrucks als String.
-            string query = @"PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-                            SELECT ?name(COUNT(?friend) AS ?count)
-                            WHERE {
-                                ?person foaf:name ?name .
-                                ?person foaf:knows ?friend .
-                            }
-                            GROUP BY ?person ?name";
-
-
-            // Ausführen der Abfrage vom Graphen
-            var queryResult = graph.ExecuteQuery(query);
-
-            // Auswerten des Ergebnisses (Typ des Results muss überprüft werden)
-            if (queryResult is SparqlResultSet)
+            try
             {
-                SparqlResultSet resultSet = (SparqlResultSet)queryResult;
+                // Laden der Daten
+                FileLoader.Load(graph, "chemicalElements.xml");
+            }
+            catch 
+            {
+                throw new IOException("An error occurred while loading the data!");
+            }
 
-                foreach (SparqlResult result in resultSet)
+            string query = @"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                             PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                             PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                             PREFIX : <http://www.semanticweb.org/josiptunjic/ontologies/2023/0/untitled-ontology-5#>
+                             SELECT DISTINCT ?subject ?object
+	                         WHERE { ?subject rdfs:subClassOf ?object. ?object rdf:type owl:Class. }";
+
+
+            // Ausführen der Abfrage vom Daten Graphen
+            try
+            {
+                var queryResult = graph.ExecuteQuery(query);
+
+                // Auswerten des Ergebnisses
+                if (queryResult is SparqlResultSet)
                 {
-                    Console.WriteLine(result.ToString());
+                    SparqlResultSet resultSet = (SparqlResultSet)queryResult;
+
+                    foreach (SparqlResult result in resultSet)
+                    {
+                        Console.WriteLine(result.ToString() + "\n");
+                    }
                 }
+            }
+            catch
+            {
+                throw new RdfParseException("An error occurred while parsing the query result!");
             }
 
             Console.ReadKey();
